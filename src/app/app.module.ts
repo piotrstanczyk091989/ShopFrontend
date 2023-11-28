5106453b5ab659215971d0a544d039b042d96633
piotrstanczyk091989 <piotr.stanczyk.09@gmail.com>
2023-11-28 23:16:45 +0100

    Created layout

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DefaultModule } from './layouts/default/default.module';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { SidebarComponent } from './shared/components/sidebar/sidebar.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+    ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    DefaultModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
