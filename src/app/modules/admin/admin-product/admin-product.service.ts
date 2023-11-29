a3087343a51c1d609c278448b921462b5527edb5
piotrstanczyk091989 <piotr.stanczyk.09@gmail.com>
2023-11-29 22:56:42 +0100

    Product list  in panel admin

---
+++ src/app/modules/admin/admin-product/admin-product.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { AdminProduct } from './adminProduct';
+import { Page } from 'src/app/shared/model/page';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminProductService {
+
+  constructor(private http: HttpClient) { }
+
+  getProducts(page: number, size: number): Observable<Page<AdminProduct>> {
+    return this.http.get<Page<AdminProduct>>(`/api/admin/products?page=${page}&size=${size}`);
+  }
+}
--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -5,6 +5,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
 import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableModule } from '@angular/material/table';
 
 
 @NgModule({
@@ -15,14 +16,16 @@ import { MatPaginatorModule } from '@angular/material/paginator';
     MatButtonModule,
     MatListModule,
     MatCardModule,
-    MatPaginatorModule
+    MatPaginatorModule,
+    MatTableModule
   ],
   exports: [
     MatIconModule,
     MatButtonModule,
     MatListModule,
     MatCardModule,
-    MatPaginatorModule
+    MatPaginatorModule,
+    MatTableModule
   ]
 })
 export class MaterialModule { }
