3ae85d71d8528b1b7ed2099eb4b382e45b546d50
piotrstanczyk091989 <piotr.stanczyk.09@gmail.com>
2023-12-10 21:59:41 +0100

    Adding and viewing reviews for products

--- src/app/modules/product-details/product-details.component.ts
+++ src/app/modules/product-details/product-details.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { ProductDetails } from './model/productDetails';
 import { ProductDetailsService } from './product-details.service';
 import { ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Review } from './model/review';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-product-details',
@@ -10,18 +13,49 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ProductDetailsComponent implements OnInit {
   product!: ProductDetails;
+  reviewForm!: FormGroup;
 
-  constructor(private productDetailsService: ProductDetailsService,
-    private router: ActivatedRoute) { }
+  constructor(
+    private productDetailsService: ProductDetailsService,
+    private router: ActivatedRoute,
+    private formBuilder: FormBuilder,
+    private snackBar: MatSnackBar
+    ) { }
 
   ngOnInit(): void {
-    this.getProductDetails()
+    this.getProductDetails();
+    this.reviewForm =  this.formBuilder.group({
+      authorName: ['', [Validators.required, Validators.minLength(2)]],
+      content: ['', [Validators.required, Validators.minLength(2)]]
+    })
   }
 
   getProductDetails() {
     let slug = this.router.snapshot.params['slug'];
     this.productDetailsService.getProductDetails(slug)
       .subscribe(product => this.product = product);
+  }
+  
+  submit(){
+    if(this.reviewForm.valid){
+      this.productDetailsService.saveProductReview({
+        authorName: this.reviewForm.get("authorName")?.value,
+        content: this.reviewForm.get("content")?.value,
+        productId: this.product.id
+      }as Review).subscribe(review => {
+        this.reviewForm.reset();
+        this.snackBar.open('Dziękuje za dodanie opinii','', {duration: 3000, panelClass: "snack-bar-bg-color-ok"});
+      });
+    }
+  }
+
+  get authorName(){
+    return this.reviewForm.get('authorName');
+  }
+
+
+  get content(){
+    return this.reviewForm.get('content');
   }
 
 }
--- src/app/modules/product-details/product-details.service.ts
+++ src/app/modules/product-details/product-details.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ProductDetails } from './model/productDetails';
 import { Observable } from 'rxjs';
+import { Review } from './model/review';
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +13,9 @@ export class ProductDetailsService {
 
   getProductDetails(slug: string): Observable<ProductDetails>{
     return this.http.get<ProductDetails>("/api/products/" + slug)
+  }
+
+  saveProductReview(review: Review): Observable<Review>{
+    return this.http.post<Review>("/api/reviews", review);
   }
 }
