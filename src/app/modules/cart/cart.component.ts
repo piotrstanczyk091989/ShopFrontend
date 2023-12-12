95faa65239b93fbdd409e170e5bf4d5d2d5d1069
piotrstanczyk091989 <piotr.stanczyk.09@gmail.com>
2023-12-12 21:41:09 +0100

    Cart update

--- src/app/modules/cart/cart.component.ts
+++ src/app/modules/cart/cart.component.ts
@@ -3,6 +3,8 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { CartService } from './cart.service';
 import { CartSummary } from './model/cartSummary';
 import { CookieService } from 'ngx-cookie-service';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
+import { CartSummaryItem } from './model/cartSummaryItem';
 
 @Component({
   selector: 'app-cart',
@@ -11,13 +13,15 @@ import { CookieService } from 'ngx-cookie-service';
 })
 export class CartComponent implements OnInit {
 
+  formGroup!: FormGroup;
   summary!: CartSummary;
 
   constructor(
     private route: ActivatedRoute,
     private cartService: CartService,
     private cookieService: CookieService,
-    private router: Router
+    private router: Router,
+    private formBuilder: FormBuilder
     ) { }
 
   ngOnInit(): void {
@@ -27,13 +31,20 @@ export class CartComponent implements OnInit {
     } else {
       this.getCart();
     }
+
+    this.formGroup = this.formBuilder.group({
+      items: this.formBuilder.array([])
+    });
   }
 
   getCart(){
     let cartId = Number(this.cookieService.get("cartId"));
     if(cartId > 0){
       this.cartService.getCart(cartId)
-      .subscribe(summary => this.summary = summary)
+      .subscribe(summary => {
+        this.summary = summary
+        this.patchFormItems();
+      })
     }
 
   }
@@ -43,14 +54,53 @@ export class CartComponent implements OnInit {
     this.cartService.addToCart(cartId, {productId: id, quantity: 1})
     .subscribe(summary => {
       this.summary = summary;
+      this.patchFormItems();
       this.cookieService.delete("cartId");
       this.cookieService.set("cartId", summary.id.toString(), this.expiresDays(3));
       this.router.navigate(["/cart"]);
     });
   }
 
+  patchFormItems(){
+    let formItems = <FormArray>this.formGroup.get("items");
+    this.summary.items.forEach(item => {
+      formItems.push(this.formBuilder.group({
+        id: [item.id],
+        quantity: [item.quantity],
+        product: [item.product],
+        lineValue: [item.lineValue]
+      }));
+    })
+  }
+
   expiresDays(days: number): Date {
     return new Date(Date.now() + days * 24 * 60 * 60 * 1000)
+  }
+
+  submit(){
+    let cartId = Number(this.cookieService.get("cartId"));
+    this.cartService.updateCart(cartId, this.mapToRequestListDto())
+    .subscribe(summary => {
+      this.summary = summary
+      this.formGroup.get("items")?.setValue(summary.items)
+    });
+  }
+
+  mapToRequestListDto(): any[] {
+    let items: Array<CartSummaryItem> = this.formGroup.get("items")?.value;
+    return items.map(item => ({
+      productId: item.product.id,
+      quantity: item.quantity,
+    }))
+  }
+
+  deleteItem(itemId: number){
+    this.cartService.deleteCartItem(itemId)
+    .subscribe(() => this.ngOnInit());
+  }
+
+  get items(){
+    return (<FormArray>this.formGroup.get("items")).controls;
   }
 
 }
--- src/app/modules/cart/cart.service.ts
+++ src/app/modules/cart/cart.service.ts
@@ -17,4 +17,13 @@ export class CartService {
   addToCart(id: number, cartItem: any): Observable<CartSummary> {
     return this.http.put<CartSummary>("/api/carts/" + id, cartItem);
   }
+
+  updateCart(id: number, items: any[]): Observable<CartSummary>  {
+    return this.http.put<CartSummary>("/api/carts/" + id + "/update" , items);
+  }
+
+  deleteCartItem(itemId: number): Observable<void> {
+    return this.http.delete<void>("/api/cartItems/" + itemId);
+  }
+
 }
