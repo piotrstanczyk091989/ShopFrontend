57b4153d55b1595953a672efc801421394742d02
piotrstanczyk091989 <piotr.stanczyk.09@gmail.com>
2023-12-18 21:19:28 +0100

    User injection in the order process

--- src/app/modules/order/order.component.ts
+++ src/app/modules/order/order.component.ts
@@ -7,6 +7,7 @@ import { OrderDto } from './model/orderDto';
 import { OrderSummary } from './model/orderSummary';
 import { InitData } from './model/initData';
 import { CartIconService } from '../common/service/cart-icon.service';
+import { JwtService } from '../common/service/jwt.service';
 
 @Component({
   selector: 'app-order',
@@ -20,6 +21,7 @@ export class OrderComponent implements OnInit {
   orderSummary!: OrderSummary;
   initData!: InitData;
   errorMessage = false;
+  isLoggedIn = false;
 
   private statuses = new Map<string, string>([
     ["NEW", "Nowy"]
@@ -29,7 +31,8 @@ export class OrderComponent implements OnInit {
     private cookieService: CookieService,
     private orderService: OrderService,
     private formBuilder: FormBuilder,
-    private cartIconService: CartIconService
+    private cartIconService: CartIconService,
+    private jwtService: JwtService
   ) { }
 
   ngOnInit(): void {
@@ -46,6 +49,7 @@ export class OrderComponent implements OnInit {
       payment: ['', Validators.required]
     });
     this.getInitData();
+    this.isLoggedIn = this.jwtService.isLoggedIn();
   }
 
   checkCartEmpty() {
